0c37063994c7fbebfa8a976b6021e62acb7d314d
DENRIG777 <62824680+DENRIG777@users.noreply.github.com>
2021-12-07 22:21:30 -0600

    added content

--- js/site.js
+++ js/site.js
@@ -16,7 +16,7 @@ function getValues() {
         let calc = calculatePayments(loanamount, payments, rate);
     
         //call view
-        displayloan(calc, loanamount, payments);
+        displayLoan(calc, loanamount, payments);
     }
 }
 
@@ -38,14 +38,58 @@ function calculatePayments(la, p, r) {
     //push the starting loan amount to current balance
     currentBalance.push(la);
 
+    //loop to calculate each month amounts and push each value into it's array
+    for (let i = 0; i < p; i++) {
+        interestPayment.push(currentBalance[i] * r/1200);
+        principalPayment.push(totalMonthlyPayments - interestPayment[i]);
+        remainingBalance.push(currentBalance[i] - principalPayment);
 
+        currentBalance.push(remainingBalance[i]);
+    }
 
+    // add each array to the calc obj
+    calc.currentBalance = currentBalance;
+    calc.interestPayment = interestPayment;
+    calc.principalPayment = principalPayment;
+    calc.remainingBalance = remainingBalance;
+    calc.totalMonthlyPayments = totalMonthlyPayments;
 
-
-
-
-
-
-
-
+    return calc;
 } 
+
+
+//display payments and amortization table
+//view
+function displayLoan(calc, la, p) {
+    document.getElementById("monthlyPayment").innerHTML =`${calc.totalMonthlyPayment.toFixed(2)}`;
+    document.getElementById("totalPrincipal").innerHTML =`${la.toFixed(2)}`;
+
+    //get result area and template for use
+    let tableBody = document.getElementById("results");
+    let templateRow = document.getElementById("template");
+    let totalInterest = 0;
+
+    // loop array to create output from array
+    for (let i = 0; i < p; i++)
+    {
+        let tableRow = document.importNode(templateRow.content, true);
+
+        //sum the interest for each month
+        totalInterest += calc.interestPayment[i];
+
+        //grab the td in an array 
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].textContent = i+1;
+        rowCols[1].textContent = `${calc.totalMonthlyPayment.toFixed(2)}`;
+        rowCols[2].textContent = `${calc.principalPayment[i].toFixed(2)}`;
+        rowCols[3].textContent = `${calc.interestPayment[i].toFixed(2)}`;
+        rowCols[4].textContent = `${totalInterest.toFixed(2)}`;
+        rowCols[5].textContent = `${calc.remainingBalance[i].toFixed(2)}`;
+
+        tableBody.appendChild(tableRow);
+    }
+    //add the last items for total interest and total cost of completed loan 
+    document.getElementById("totalInterest").innerHTML =`${totalInterest.toFixed(2)}`;
+    let totalCost = totalInterest + la;
+    document.getElementById("totalCost").innerHTML =`${totalCost.toFixed(2)}`;
+}
